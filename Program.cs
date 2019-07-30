0ef9f3c5f5e7726ab1acd4a148ad146eaa957d71
MichaelRAubin <mr-aubin@hotmail.com>
2019-07-29 20:51:19 -0600

    .Net Introduction

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PrimativeTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to Dotnet 101!");
+            Console.WriteLine("What is your name?");
+            string name = Console.Readline();
+            Console.WriteLine($"Welcome to class, {name}!");
+            Console.WriteLine("How old are you?");
+            string strAge = Console.Readline();
+            Int32.TryParse(strAge, out int age);
+            System.Console.WriteLine($"In ten years {name} will be {age + 10}.");
+
+            //var userInput = Console.Readline();
+
+
+
+
+
+
+
+
+            // int age = 24;
+            // decimal bankAccount = 10.75m;
+            // float sisterAge = 22f;
+            //bool imTrue = true;
+        }
+    }
+}
